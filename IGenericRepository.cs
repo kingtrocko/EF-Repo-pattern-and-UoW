6368822e827bf493206686bf6b94b293d49ef8ce
kingtrocko <nahum_fabian13@hotmail.com>
2014-08-26 07:08:03 -0700

    Delete IGenericRepository.cs

--- IGenericRepository.cs
+++
@@ -1,9 +0,0 @@
-public interface IGenericRepository<T> where T : class {
-    
-    IQueryable<T> GetAll();
-    IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
-    void Add(T entity);
-    void Delete(T entity);
-    void Edit(T entity);
-    void Save();
-}
